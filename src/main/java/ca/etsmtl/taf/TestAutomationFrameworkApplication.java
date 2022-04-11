266a16796d903a28968e6b0d22e95dce32a161a3
Mohammed Hilali <mohammed.hilali.1@etsmtl.net>
2022-04-11 00:24:35 -0400

    Initital Commit (Project Structure)

---
+++ src/main/java/ca/etsmtl/taf/TestAutomationFrameworkApplication.java
@@ -0,0 +1,13 @@
+package ca.etsmtl.taf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutomationFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ca/etsmtl/taf/access/entity/User.java
@@ -0,0 +1,24 @@
+package ca.etsmtl.taf.access.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+	
+	private String userName;
+	
+    private String fullName;
+    
+    private String email;
+}
