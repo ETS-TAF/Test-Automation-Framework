f58fc8c7ab4b05226febab444a6533808b3b4c41
Mohammed Hilali <mohammed.hilali.1@etsmtl.net>
2022-04-17 15:30:54 -0400

    ETS Style

--- src/main/java/ca/etsmtl/taf/config/WebSecurityConfig.java
+++ src/main/java/ca/etsmtl/taf/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/css/**", "/js/**", "/registration").permitAll()
+                .antMatchers("/css/**", "/images/**","/js/**", "/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
