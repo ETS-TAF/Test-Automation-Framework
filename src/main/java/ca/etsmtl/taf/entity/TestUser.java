054da1f911d99d49b7f01730a1213205fb865547
Mohammed Hilali <mohammed.hilali.1@etsmtl.net>
2022-04-22 21:01:24 -0400

    Audit infos & local config setup

--- src/main/java/ca/etsmtl/taf/entity/TestUser.java
+++ src/main/java/ca/etsmtl/taf/entity/TestUser.java
@@ -1,12 +1,10 @@
 package ca.etsmtl.taf.entity;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -15,19 +13,14 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "t_project")
-public class Project {
+@Table(name = "t_test_user")
+public class TestUser {
 	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-	
-	private String name;
-	private String description;
-	private Date startDate;
-	private Date endDate;
-	
-	@ManyToOne
-	private TestUser owner;
+    
+    @OneToOne
+    private User systemUser;
 
 }
