493cce13498e21d744fb46140bc86fde3fbea372
mahdi <mahdios20182@gmail.com>
2023-10-22 11:37:23 -0400

    frontend : add actions work correctly

--- frontend/src/app/selenium/test-selenium.component.ts
+++ frontend/src/app/selenium/test-selenium.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./test-selenium.component.css']
 })
 export class TestSeleniumComponent {
+    counter: number=1;
+    actions: {
+        id: number;
+        action: string;
+        object: string;
+        input: string;
+        target: string;
+    }[] = [];
+
+    
     actionChose(): void {
         const action = (document.getElementById('action') as HTMLSelectElement).value;
         const object = document.getElementById('object') as HTMLInputElement;
@@ -27,6 +37,22 @@ export class TestSeleniumComponent {
         if (action === "Clear" || action === "Click" || action === "IsDisplayed" || action === "FillField") {
             object.disabled = false;
         }
+    }
+    submitAction(){
+        let action = (document.getElementById('action') as HTMLSelectElement).value;
+        let object = (document.getElementById('object') as HTMLInputElement).value;
+        let input = (document.getElementById('input') as HTMLInputElement).value;
+        let target = (document.getElementById('target') as HTMLInputElement).value;
+        this.addJsonObject({ id: this.counter, action: action, object: object, input: input, target: target, });
+        console.log(this.getJsonObjectById(this.counter));
+        this.counter++;
+    }
+    public addJsonObject(obj: { id: number,action: string,object: string,input: string,target: string }) {
+        this.actions.push(obj);
+    }
+    
+    public getJsonObjectById(id: number) {
+        return this.actions.find(obj => obj.id === id);
     }
     
 }
